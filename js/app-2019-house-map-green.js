3f23eee3396349eeb9c1e91265b9e1926b0a048f
Marjorie Roswell <mroswell@gmail.com>
2020-06-18 17:48:20 -0400

    updates

--- js/app-2019-house-map-green.js
+++ js/app-2019-house-map-green.js
@@ -201,19 +201,19 @@ document.getElementById("buttonState").addEventListener("click", function () {
 document.getElementById("buttonPittsburgh").addEventListener("click", function () {
     map.flyTo([40.43, -79.82], 9.25, {
         animate: true,
-        duration: 1.6 // in seconds
+        duration: 1.4 // in seconds
     });
 });
 
 document.getElementById("buttonPhiladelphia").addEventListener("click", function () {
     map.flyTo([40, -75.4], 9.25, {
         animate: true,
-        duration: 1.6 // in seconds
+        duration: 1.4 // in seconds
     });
 });
 document.getElementById("buttonAllentown").addEventListener("click", function () {
     map.flyTo([41, -75.5], 9, {
         animate: true,
-        duration: 1.6 // in seconds
+        duration: 1.4 // in seconds
     });
 });
